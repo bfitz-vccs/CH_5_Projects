707c0fd88e3b6351705f3b995d9ec08ff7776d22
bfitz-vccs <bf2877@email.vccs.edu>
2025-07-20 17:42:10 -0400

    Changed asterisks to pluses.

--- patterns.cpp
+++ patterns.cpp
@@ -5,14 +5,14 @@ int main(){
     std::cout << "Pattern A" << std::endl;
     for (int i = 1; i < 11; i++){
         for (int j = 0; j < i; j++){
-            std::cout << "*";
+            std::cout << "+";
         }
         std::cout << std::endl;
     }
     std::cout << "Pattern B" << std::endl;
     for (int i = 10; i > 0; i--){
         for (int j = i; j > 0; j--){
-            std::cout << "*";
+            std::cout << "+";
         }
         std::cout << std::endl;
     }
